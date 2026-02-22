98045f077b515ae8959e6987a68da54a1c11ea5c
whitecy01 <whitecy01@naver.com>
2026-02-22 20:52:15 +0900

    feat: DelegatingAuthenticationConverter의 전략 패턴

--- src/main/java/com/oauth/auth_server/springauthserver/web/authentication/OAuth2AuthorizationCodeRequestAuthenticationConverter.java
+++ src/main/java/com/oauth/auth_server/springauthserver/web/authentication/OAuth2AuthorizationCodeRequestAuthenticationConverter.java
@@ -2,13 +2,33 @@ package com.oauth.auth_server.springauthserver.web.authentication;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 public final class OAuth2AuthorizationCodeRequestAuthenticationConverter implements AuthenticationConverter {
 
+    private final RequestMatcher requestMatcher = createDefaultRequestMatcher();
+
+
+    private static RequestMatcher createDefaultRequestMatcher(){
+        RequestMatcher getMethodMatcher = (request) -> "GET".equals(request.getMethod());
+        RequestMatcher postMethodMatcher = (request) -> "POST".equals(request.getMethod());
+        RequestMatcher responseTypeParameterMatcher = (
+                request) -> request.getParameter(OAuth2ParameterNames.RESPONSE_TYPE) != null;
+        return new OrRequestMatcher(getMethodMatcher,
+                new AndRequestMatcher(postMethodMatcher, responseTypeParameterMatcher));
+    }
 
     @Override
     public Authentication convert(HttpServletRequest request) {
+
+        if (!this.requestMatcher.matches(request)){
+            return null;
+        }
+
         return null;
     }
 }
